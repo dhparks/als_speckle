272ba234fd206ea3d1ffd8564937e93740537c52
Keoki Seu <kaseu@lbl.gov>
2012-05-22 15:36:41 -0700

    file that allows us to setup speckle as a module. Running: python setup.py install
    will now install speckle as a module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+dependencies = ['scipy', 'numpy']
+
+setup(
+    name='speckle',
+    version='0.1',
+    py_modules=['speckle'],
+    requires = dependencies,
+    author = "Daniel Parks, Keoki Seu",
+    author_email = "dhparks@lbl.gov, kaseu@lbl.gov",
+    description = "Library for speckle analysis at Beamline 12.0.2 at the Advanced Light Source",
+    url = "https://sites.google.com/a/lbl.gov/coherent-scattering-beamline/"
+)
