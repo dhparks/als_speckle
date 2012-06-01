f1cf409e20ffdec42d31916582e1e87871631540
Keoki Seu <kaseu@lbl.gov>
2012-06-01 12:47:34 -0700

    Add a header describing the library. Also add author list and contact info.

--- speckle/__init__.py
+++ speckle/__init__.py
@@ -1,3 +1,17 @@
+""" A speckle correnation library for Beamline 12.0.2 at the Advanced Light
+Source at Lawrance Berkeley National Laboratory. This library contains
+functions that allow the user to analyze data from experiments conducted at
+the ALS. This includes magnetic memory, rotational symmetries, imaging, near
+field propagation, and x-ray photon correlation spectroscopy.
+
+The beamline webpages are here:
+http://ssg.als.lbl.gov/ssgbeamlines/beamline12-0-2
+https://sites.google.com/a/lbl.gov/coherent-scattering-beamline/
+
+Author: Keoki Seu (kaseu@lbl.gov)
+Author: Daniel Parks (dhparks@lbl.gov)
+
+"""
 # if you make a new file/module name, put it here
 __all__ = ["fit",
            "io",
