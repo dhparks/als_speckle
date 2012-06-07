bdc45d3811a49b7132f651e89b02be1c17ec2f5e
Keoki Seu <kaseu@lbl.gov>
2012-06-07 09:44:07 -0700

    in shape.py: -Don't need to use scipy.  Convert all scipy calls to bumpy. -rewrite all of the documentation to be standardized. -In square(), just call rect() instead of doing everything over.  Less maintenance. -in circle(), remove the argument bessel=False.  It's not used and (more importantly) not documented! -Change some of the asserts to be more liberal.  For example, in rect() col_length doesn't need to be int. It can be float but just cast it to int.

--- speckle/shape.py
+++ speckle/shape.py
@@ -1,56 +1,43 @@
-#shapes
-#	circle/ellipse (anti-aliased)
-#	square/rect
-#	gaussian
-#	annulus
-#	radial/angular
-
-import scipy
+import numpy
 
 def radial(size,center=None):
+    """ Returns azimuthally symmetric distance function in n-dimensions
     
-    """
-    Returns azimuthally symmetric distance function in n-dimensions
-    
-    Required input:
-    size: an n-tuple for the array size in format (rows,columns).
-          must be integer.
-    
-    Optional input:
-    center: an n-tuple giving origin of coordinate system (ie, where r = 0).
-            Defaults to array center in absence of supplied values.
-            Boundary conditions are NOT cyclic.
-    """
-
-    # check types
+    arguments:
+        size: an n-tuple for the array size in format (rows,columns).
+              Must be integer.
+        center: an n-tuple giving origin of coordinate system (ie, where r = 0).
+                Defaults to array center in absence of supplied values.
+                Boundary conditions are NOT cyclic.
+    returns:
+        A n-dimensioanl array where each element is the distance (in pixels) from the center.
+   """
     assert type(size) is tuple, "size must be a tuple"
     
     if center != None:
         assert isinstance(center,tuple), "center must be supplied as a tuple"
         assert len(center) == len(size), "size and center must be same dimensionality"
     else:
-        center = scipy.zeros(len(size),float)
+        center = numpy.zeros(len(size),float)
         for d in range(len(size)): center[d] = size[d]/2.
         
     # evaluate pythagorean theorem in n-dimensions
-    indices = scipy.indices(size,float)
-    r = scipy.zeros_like(indices[0])
+    indices = numpy.indices(size,float)
+    r = numpy.zeros_like(indices[0])
 
-    if len(size) == 2: return scipy.hypot(indices[0]-center[0],indices[1]-center[1])
+    if len(size) == 2: return numpy.hypot(indices[0]-center[0],indices[1]-center[1])
     else:
         for d in range(len(size)): r += (indices[d]-center[d])**2
-        return scipy.sqrt(r)
+        return numpy.sqrt(r)
     
-def angular(size,center=None):
+def angular(size,center=None):    
+    """ Generate a radially symmetric angle function.
     
-    """
-    Returns radially symmetric angle function by means of atan2 (?)
-    
-    Required input:
-    size: the size of the array as a tuple (rows,columns).
-    
-    Optional inputs:
-    center: the center of the coordinate system as a tuple (center_row,center_column)
+    arguments:
+        size: the size of the array as a tuple (rows,columns).
+        center: the center of the coordinate system as a tuple (center_row,center_column)
+    returns:
+        A 2-dimensional array of the angular values, in radians.
     """
 
     assert isinstance(size,tuple) and len(size) == 2, "size must be a 2-tuple"
@@ -58,48 +45,55 @@ def angular(size,center=None):
     if center == None: center = (size[0]/2,size[1]/2)
     assert isinstance(center,tuple) and len(center) == 2, "center must be a 2-tuple"
 
-    rows,cols = scipy.indices(size,float)
+    rows,cols = numpy.indices(size,float)
     rows += -center[0]
     cols += -center[1]
-    return scipy.angle(cols+complex(0,1)*rows)
+    return numpy.angle(cols+complex(0,1)*rows)
     
 def square(size,length,center=None):
+    """ Generate a square in a numpy array.
     
-    # check types
-    
-    assert isinstance(size,tuple) and len(size) == 2, "size must be a 2-tuple"
-    assert isinstance(length,int), "length must be an integer"
-    
-    if center == None: center = (size[0]/2,size[1]/2)
-    assert isinstance(center,tuple) and len(center) == 2, "center must be a 2tuple"
-    
-    temp = scipy.zeros(size,int)
-    temp[center[0]-length/2:center[0]+length/2,center[1]-length/2:center[1]+length/2] = 1
-    return temp
+    arguments:
+        size: the size of the array as a tuple (rows,columns).
+        length: length of the square.
+        center: the center of the coordinate system as a tuple (center_row,center_column)
+    returns:
+        A 2-dimensional numpy array with a square of length (length) centered at (center).
+    """
+    return rect(size, length, legnth, center)
     
 def rect(size,row_length,col_length,center=None):
+    """ Generate a rectangle in a numpy array.
     
+    arguments:
+        size: the size of the array as a tuple (rows,columns).
+        row_length: row length of the rectangle.
+        col_length: col length of the rectangle.
+        center: the center of the coordinate system as a tuple (center_row,center_column)
+    returns:
+        A 2-dimensional numpy array with a rectangle of (row_length, col_length) centered at (center).
+    """
     if center == None: center = (size[0]/2,size[1]/2)
 
     assert isinstance(size,tuple) and len(size) == 2, "size must be a 2-tuple"
-    assert type(row_length) is int and type(col_length) is int, "lengths must be integer"
+    assert type(row_length) in (float, int) and type(col_length) in (float, int), "lengths must be float or integer"
+    row_length = int(row_length)
+    col_length = int(col_length)
     
-    temp = scipy.zeros(size,int)
+    temp = numpy.zeros(size,int)
     temp[center[0]-row_length/2:center[0]+row_length/2,center[1]-col_length/2:center[1]+col_length/2] = 1
     return temp
     
-def circle(size,radius,center=None,AA=True,bessel=False):
+def circle(size,radius,center=None,AA=True):
+    """ Generate a circle in a numpy array.
     
-    """
-    Returns a circle.
-    
-    Required input:
-    1. size: a 2-tuple formatted as (rows,columns)
-    2. radius of circle
-    
-    Optional input:
-    center: a 2-tuple of (center_row,center_column) where the circle is centered
-    AA: if AA is True, returns an antialiased circle. AA = False gives a jagged edge and is marginally faster
+    arguments:
+        size: a 2-tuple formatted as (rows,columns)
+        radius of circle
+        center: a 2-tuple of (center_row,center_column) where the circle is centered
+        AA: if AA is True, returns an antialiased circle. AA = False gives a jagged edge and is marginally faster
+    returns:
+        a numpy array of size with a circle of radius centered on center.
     """
     
     # check types
@@ -111,20 +105,19 @@ def circle(size,radius,center=None,AA=True,bessel=False):
 
     r = radial(size,center)
     
-    if not AA: return scipy.where(r < radius,1,0)
-    if AA: return 1-scipy.clip(r-radius,0,1.) 
+    if not AA: return numpy.where(r < radius,1,0)
+    if AA: return 1-numpy.clip(r-radius,0,1.) 
     
 def annulus(size,radii,center=None,AA=True):
-    """ Returns an annulus (ie a ring)
-    
-    Required input:
-    size: size of array (rows,columns). tuple entries must be int.
-    radii: interior and exterior radius of the annulus as a tuple. order is not important because smaller radius must be the interior.
-    
-    Optional input:
-    center: a 2-tuple of (center_row,center_column) where the annulus is centered
-    AA: if True, antialiases the annulus.
-    
+    """ Returns an annulus (ie a ring) in a numpy array.
+
+    arguments:
+        size: size of array (rows,columns). tuple entries must be int.
+        radii: interior and exterior radius of the annulus as a tuple. The order is not important because smaller radius must be the interior.
+        center: a 2-tuple of (center_row,center_column) where the annulus is centered
+        AA: if True, antialiases the annulus.
+    returns:
+        a numpy array with annuls centered on center of radius (r_in, r_out)    
     """
     assert type(radii) is tuple and type(radii[0]) in (int,float) and type(radii[1]) in (int,float), "radius must be a 2-tuple of floats or ints"
     # no need to do other asserts, circle() takes care of it.
@@ -132,15 +125,15 @@ def annulus(size,radii,center=None,AA=True):
     return circle(size,max(radii),center,AA)-circle(size,min(radii),center,AA)
 
 def ellipse(size,axes,center=None,AA=True):
-    """ Returns an ellipse
+    """ Returns an ellipse in a numpy array.
     
-    Required input:
-    size: size of array, 2-tuple (rows,columns)
-    axes: (vertical "radius", horizontal "radius")
-    
-    Optional input:
-    center: 2-tuple recentering coordinate system to (row,column)
-    AA: if True, anti-aliases the edge of the ellipse
+    arguments:
+        size: size of array, 2-tuple (rows,columns)
+        axes: (vertical "radius", horizontal "radius")
+        center: 2-tuple recentering coordinate system to (row,column)
+        AA: if True, anti-aliases the edge of the ellipse
+    returns:
+        numpy array with an ellipse drawn.
     """
     
     # check types
@@ -154,7 +147,7 @@ def ellipse(size,axes,center=None,AA=True):
     assert type(AA) is bool or AA in (0,1), "AA value must be bool-evaluable"
     
     # we can do this like a circle by stetching the coordinates along an axis
-    rows,cols = scipy.indices(size).astype('float')
+    rows,cols = numpy.indices(size).astype('float')
     rows += -center[0]
     cols += -center[1]
     
@@ -167,20 +160,20 @@ def ellipse(size,axes,center=None,AA=True):
         radius = axes[0]
     
     # now with the stretched coordinate system the evaluation is just like that for a circle
-    r = scipy.hypot(rows,cols)
-    if not AA: return scipy.where(r < radius,1,0)
-    if AA: return 1-scipy.clip(r-radius,0,1)
+    r = numpy.hypot(rows,cols)
+    if not AA: return numpy.where(r < radius,1,0)
+    if AA: return 1-numpy.clip(r-radius,0,1)
         
 def gaussian(size,lengths,center=None,normalization=None):
     """ Returns an 1-dimensional or 2-dimensional gausssian.
     
-    Required input:
-    size: size of array (rows,columns). must be int.
-    lengths: stdevs of gaussian (rows,columns). float or int.
-    
-    Optional input:
-    center: recenter coordinate system to (row,column).
-            Boundary conditions are NOT cyclic.
+    arguments:
+        size: size of array (rows,columns). must be int.
+        lengths: stdevs of gaussian (rows,columns). float or int.
+        center: recenter coordinate system to (row,column).
+                Boundary conditions are NOT cyclic.
+    returns:
+        numpy array (1d or 2d) with a gaussian of the given parameters.
     """
     
     # check types
@@ -195,7 +188,7 @@ def gaussian(size,lengths,center=None,normalization=None):
         assert len(center) == len(size), "size and center must be same dimensionality"
         for d in range(len(center)): assert type(center[d]) in (int,float), "center values must be float or int"
     else:
-        center = scipy.zeros_like(size)
+        center = numpy.zeros_like(size)
         for d in range(len(center)): center[d] = size[d]/2.
 
     if normalization is not None:
@@ -204,23 +197,23 @@ def gaussian(size,lengths,center=None,normalization=None):
     # now build the gaussian.
     
     if len(size) == 1:
-        x = scipy.arange(size[0]).astype('float')
+        x = numpy.arange(size[0]).astype('float')
         s = lengths[0]
         c = center[0]
-        gaussian = scipy.exp(-(x-c)**2/(2*s**2))
+        gaussian = numpy.exp(-(x-c)**2/(2*s**2))
         
     if len(size) == 2:
-        x = scipy.arange(size[0]).astype('float')
+        x = numpy.arange(size[0]).astype('float')
         s = lengths[0]
         c = center[0]
-        gaussian1 = scipy.exp(-(x-c)**2/(2*s**2))
+        gaussian1 = numpy.exp(-(x-c)**2/(2*s**2))
         
-        x = scipy.arange(size[1]).astype('float')
+        x = numpy.arange(size[1]).astype('float')
         s = lengths[1]
         c = center[1]
-        gaussian2 = scipy.exp(-(x-c)**2/(2*s**2))
+        gaussian2 = numpy.exp(-(x-c)**2/(2*s**2))
         
-        gaussian = scipy.outer(gaussian1,gaussian2)
+        gaussian = numpy.outer(gaussian1,gaussian2)
             
     if normalization == None: return gaussian
-    else: return gaussian*float(normalization)/(scipy.sum(gaussian))
+    else: return gaussian*float(normalization)/(numpy.sum(gaussian))
