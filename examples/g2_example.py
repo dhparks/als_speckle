65c245eaa6e11bd6ab6c1a370892b03a95bc7db4
Keoki Seu <kaseu@lbl.gov>
2012-06-26 20:38:49 -0700

    Get rid of directories for examples, we'll have so few that it doesn't make sense to have them. Add two examples for g2, one for single photon and another for regular g2.

---
+++ examples/g2_example.py
@@ -0,0 +1,26 @@
+import speckle
+
+datafile = "../pyc-g2/srt43-small.fits"
+data = speckle.io.openfits(datafile)
+
+# Draw a ROI in ds9. in DS9 select Region->Shape->Polygon and draw a region.
+# Save the region with Region->Save Regions
+mask = speckle.io.open_ds9_mask('srt43-ROI.reg')
+
+# or if you know the ROI as a 40x40 box centered at Row, Col (236, 118)
+#mask = speckle.shape.rect(data[0].shape,40, 40, (236,118))
+
+# Optionally, convert the movie to photons before calculating g2.
+#data = speckle.scattering.ccd_to_photons(data, 778.1)
+
+# calculate 100 frames of an XPCS movie (there are many other normalizations)
+g2 = speckle.xpcs.g2_symm_norm(data, 100)
+
+# optionally, save the g2 file once it's calculated so we can average later.
+#speckle.io.writefits("srt43-g2.fits", g2)
+
+# average g2 within the mask
+avg_g2 = speckle.averaging.calculate_average(g2, mask)
+
+# write to disk.
+speckle.io.write_text_array("srt43-g2.txt", avg_g2)
---
+++ examples/single_photon_g2_example.py
@@ -0,0 +1,29 @@
+import speckle
+import numpy
+
+pcfile = "4000V_Xray4.fits"
+
+# open a photon counting fits file, correcting overflows and sorting the data by increasing incidence times
+data = speckle.io.open_photon_counting_fits(pcfile, correct=True, sort=True)
+
+# The single photon functions are in speckle.xpcs.sp_*
+
+# bin data into frames of 1e-3 s each and 32 x 32.
+# this can take a while and a large amount of ram
+#binned_data = speckle.xpcs.sp_bin_by_space_and_time(data, 1e-3, 32)
+
+# Sum all of the data into one image.
+sum_all = speckle.xpcs.sp_sum_bin_all(data, xybin=1)
+
+# write summed file
+speckle.io.writefits("4000V_Xray4-summed.fits", sum_all)
+
+# correlate the photons that are incident on x=[500:800] and y=[1400:1800]
+corr, corrtime = speckle.xpcs.sp_autocorrelation_range(data, (500,800), (1400,1800))
+
+corr_combined = numpy.column_stack((corrtime, corr))
+
+# convert to clock time by multiplying by clock frequency
+corr_combined[:, 0] = corr_combined[:, 0] * 40e-9
+
+speckle.io.write_text_array("4000V_Xray4-cc.txt", corr_combined)
