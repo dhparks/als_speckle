38acf81964ddc5ac4911c1c4ccbd6710f93dca94
Daniel Parks <dparks@uoregon.edu>
2012-06-30 13:17:40 -0700

    symmetries: de scipy-ed. added cpu symmetry microscope class wrapping: rewrite to bring unwrap_plan into better agreement with coding standards for the rest of the library there were a lot of conflicts due to parallel changes and I struggled to figure out how to resolve them so please check for things being broken

--- speckle/symmetries.py
+++ speckle/symmetries.py
@@ -1,7 +1,178 @@
-import numpy
-from . import wrapping, crosscorr
-DFT = numpy.fft.fft2
-IDFT = numpy.fft.ifft2
+""" CPU-based rotational symmetry code
+
+Author: Daniel Parks (dhparks@lbl.gov)"""
+
+import numpy as np
+from . import shape, wrapping, crosscorr
+DFT = np.fft.fft2
+IDFT = np.fft.ifft2
+
+class microscope():
+    
+    """ A class containing methods helpful in running a symmetry microscope analysis
+    on an image. For now, this class is limited to simulations where the object being
+    analyzed is square. Furthermore, speckle patterns are simulated at the same size
+    as the object."""
+    
+    def __init__(self,object=None,unwrap=None,pinhole=None,coherence=None,components=None):
+        
+        # the class is structured in such a way that its parts can all be inited right now
+        # or later on in the main script. the example will show both.
+        
+        self.can_has_object = False
+        self.can_has_unwrap = False
+        self.can_has_pinhole = False
+        self.can_has_coherence = False
+        self.cosines = None
+        
+        # if simulation information is supplied to __init__, set up those parts of the simulation.
+        # otherwise, they will have to be done later from the main script.
+        
+        if object != None: self.set_object(object)
+        if unwrap != None: self.set_unwrap(unwrap)
+        if pinhole != None: self.set_pinhole(pinhole)
+        if coherence != None: self.set_coherence(coherence)
+        if components != None: self.set_cosines(components=components)
+        
+    def set_object(self,object):
+        """ Takes a 2d array and makes it a correctly behaving object for symmetry microscope
+        calculations (basically, make it cyclic).
+        
+        arguments:
+            object: 2d numpy array
+            
+        returns: nothing, but generates class variables"""
+        
+        assert isinstance(object,np.ndarray) and object.ndim==2, "object must be 2d array"
+        assert object.shape[0] == object.shape[1], "for now, object must be square"
+        
+        self.N = object.shape[0]
+        
+        # cyclify the object so that we can slice the object rather than roll. should probably
+        # test which one is faster...
+        #new_object = np.zeros((2*self.N,2*self.N),object.dtype)
+        #new_object[0:self.N,0:self.N] = object
+        #new_object[0:self.N,self.N:]  = object
+        #new_object[self.N:,0:self.N]  = object
+        #new_object[self.N:,self.N:]   = object
+        self.object = object
+        
+        self.can_has_object = True
+        
+    def set_unwrap(self,params):
+        """ Build and name the unwrap plan.
+        
+        Input:
+            params: unwrap_r and unwrap_R. center is assumed to be at array center
+        
+        returns: nothing, but generates class variables"""
+        
+        assert self.can_has_object, "need to init object before unwrap"
+        assert isinstance(params,(list,tuple,np.ndarray)), "unwrap params must be iterable"
+        assert len(params) == 2, "must provide exactly two parameters: unwrap_r and unwrap_R"
+        
+        ur, uR = params[0],params[1]
+        ur, uR = min([ur,uR]), max([ur,uR])
+        self.rows = (uR-ur)
+        assert ur > 0, "unwrap_r must be > 0"
+        assert uR < self.N/2, "unwrap_R exceeds simulated speckle bounds"
+        
+        self.unwrap_plan = wrapping.unwrap_plan(ur,uR,(self.N/2,self.N/2))
+        
+        self.can_has_unwrap = True
+        
+    def set_pinhole(self,pinhole):
+        
+        """Set the pinhole function.
+        
+        arguments:
+            pinhole: Either a number, in which case a circle is generated as the pinhole, or
+            an array, in which case the array is set as the pinhole. This latter option allows
+            for complicated illumination shapes to be supplied."""
+        
+        assert self.can_has_object, "need to init object before pinhole"
+        assert isinstance(pinhole,(int,float,np.ndarray)), "pinhole must be number or array"
+        
+        if isinstance(pinhole,(int,float)):
+            assert pinhole < self.N/2, "pinhole radius must be smaller than pinhole array size"
+            #self.illumination = np.fft.fftshift(shape.circle((self.N,self.N),pinhole))
+            self.illumination = shape.circle((self.N,self.N),pinhole)
+            
+        if isinstance(pinhole,np.ndarray):
+            assert pinhole.shape == self.object.shape, "supplied pinhole must be same size as supplied object"
+            self.illumination = pinhole
+            
+        self.can_has_pinhole = True
+        
+    def set_coherence(self,coherence):
+        """ Make the coherence function"""
+        
+        assert self.can_has_object, "need to init object before coherence"
+        assert isinstance(coherence,(tuple,list,np.ndarray)), "coherence needs to be tuple, list, or array"
+        
+        if isinstance(coherence,(list,tuple)):
+            assert len(coherence) == 2, "coherence must have len 2"
+            clx,cly = coherence[0],coherence[1]
+            self.coherence = np.fft.fftshift(shape.gaussian((self.N,self.N),(cly,clx)))
+        
+        if isinstance(coherence,np.ndarray):
+            assert coherence.shape == (self.N,self.N), "coherence must have NxN"
+            self.coherence = coherence
+            
+        self.can_has_coherence = True
+        
+    def set_cosines(self,components=None,cosines=None):
+        assert self.can_has_unwrap, "must set unwrap before cosines"
+        
+        if components != None:
+            assert isinstance(components,(tuple,list,np.ndarray)), "components must be iterable"
+            uL = (self.unwrap_plan.shape[1]-1)/self.rows
+            self.cosines = make_cosines(components,uL)
+            
+        if cosines != None:
+            # trust the user here...
+            self.cosines = cosines
+            
+    def run_on_site(self,dy,dx,components=None,cosines=None):
+        
+        """ Run the symmetry microscope on the site of the object described by the roll coordinates
+        dy, dx. Steps are:
+        
+        1. Roll the illumination
+        2. Make the speckle
+            2b: If coherence is specified, blur the speckle
+        3. Unwrap the speckle
+        4. Autocorrelate the unwrapped speckle
+        5. Cosine-decompose the autocorrelation
+        
+        arguments:
+            dy, dx: roll coordinates used as follows: np.roll(np.roll(d,r0,axis=0),r1,axis=1)
+            components: (optional) decompose into these components
+            cosines: (optional) precomputed cosines for speed
+        """
+        
+        assert self.can_has_object, "no object loaded"
+        assert self.can_has_pinhole, "no pinhole loaded"
+        assert self.can_has_unwrap, "no unwrap loaded"
+        assert isinstance(dy,int) and isinstance(dx,int), "site coordinates must be integer"
+        
+        # some helper functions
+        rolls = lambda d,r0,r1: np.roll(np.roll(d,r0,axis=0),r1,axis=1)
+        make_speckles = lambda x: np.fft.fftshift(abs(DFT(x))**2)
+        blur_speckle = lambda x: abs(DFT(IDFT(x)*self.coherence))
+        
+        # go through the analysis. use the rot_sym function to do the unwrapping and decomposing.
+        # if nothing is supplied for components or cosines, the defaults of rot_sym become active.
+        self.illuminated = rolls(self.illumination,dy,dx)*self.object
+        self.speckles = make_speckles(self.illuminated)
+        
+        if cosines == None: cosines = self.cosines
+        if not self.can_has_coherence:
+            self.spectrum = rot_sym(self.speckles,self.unwrap_plan,cosines=cosines)
+        
+        if self.can_has_coherence:
+            self.blurred = blur_speckle(self.speckles)
+            self.spectrum = rot_sym(self.blurred,self.unwrap_plan,cosines=cosines)
 
 def make_cosines(components,N):
     """ Generates cosines to use in cosine decompsition of an autocorrelation.
@@ -13,13 +184,13 @@ def make_cosines(components,N):
     returns:
         ndarray of shape (len(components),N) containing cosine values"""
         
-    assert isinstance(components,(tuple,list,numpy.ndarray)), "components must be iterable"
+    assert isinstance(components,(tuple,list,np.ndarray)), "components must be iterable"
     assert isinstance(N,int), "N must be int"
 
-    x = (numpy.arange(N).astype(float))*(2*numpy.pi/N)
-    cosines = numpy.zeros((len(components),N),float)
+    x = (np.arange(N).astype(float))*(2*np.pi/N)
+    cosines = np.zeros((len(components),N),float)
     for n,c in enumerate(components): cosines[n] = x*c
-    return numpy.cos(cosines)
+    return np.cos(cosines)
 
 def decompose(ac,cosines):
     
@@ -32,16 +203,16 @@ def decompose(ac,cosines):
     returns:
         cosine spectrum, shape (len(ac),len(cosines))"""
         
-    assert isinstance(ac,numpy.ndarray), "ac must be array"
-    assert isinstance(cosines,numpy.ndarray), "cosines must be array"
-    assert len(cosines[0]) == len(ac[0]), "cosines are wrong shape"
+    assert isinstance(ac,np.ndarray), "ac must be array"
+    assert isinstance(cosines,np.ndarray), "cosines must be array"
+    assert len(cosines[0]) == len(ac[0]), "cosines are wrong shape (c %s ac %s)"%(len(cosines[0]),len(ac[0]))
     
     N = float(len(ac[0]))
-    decomposition = numpy.zeros((len(ac),len(cosines)),float)
+    decomposition = np.zeros((len(ac),len(cosines)),float)
     for y,row in enumerate(ac):
         for x, cosine in enumerate(cosines):
-            decomposition[y,x] = numpy.sum(row*cosine)
-    return decomposition
+            decomposition[y,x] = np.sum(row*cosine)
+    return decomposition/float(N)
 
 def rot_sym(speckles,plan=None,components=None,cosines=None):
     """ Given a speckle pattern, decompose its angular autocorrelation into a cosine series.
@@ -55,10 +226,10 @@ def rot_sym(speckles,plan=None,components=None,cosines=None):
             describing the range of radii to be analyzed. If nothing is supplied, the unwrapping
             will by default be as extensive as possible: (r,R) = (0,N/2).
             
-        components: an iterable set of integers describing which cosine components to analyze.
+        components: (optional) an iterable set of integers describing which cosine components to analyze.
             If nothing is supplied, this will be all even numbers between 2 and 20.
             
-        cosines: an ndarray containing precomputed cosines. for speed. if cosines is supplied,
+        cosines: (optional) an ndarray containing precomputed cosines. for speed. if cosines is supplied,
             components is ignored.
             
         returns:
@@ -66,15 +237,15 @@ def rot_sym(speckles,plan=None,components=None,cosines=None):
             decomposition."""
             
     # check types
-    assert isinstance(speckles,numpy.ndarray) and speckles.ndim == 2, "input data must be 2d array"
-    assert isinstance(plan,(numpy.ndarray,tuple,list,type(None))), "plan type is unrecognized"
-    assert isinstance(components,(numpy.ndarray,tuple,list,type(None))), "components are non-iterable"
+    assert isinstance(speckles,np.ndarray) and speckles.ndim == 2, "input data must be 2d array"
+    assert isinstance(plan,(np.ndarray,tuple,list,type(None))), "plan type is unrecognized"
+    assert isinstance(components,(np.ndarray,tuple,list,type(None))), "components are non-iterable"
     
     N,M = speckles.shape
     R = min([N,M])
     
     # do the unwrapping. behavior depends on what comes in as plan
-    if isinstance(plan,numpy.ndarray):
+    if isinstance(plan,np.ndarray):
         unwrapped = wrapping.unwrap(speckles,plan)
     if isinstance(plan,tuple):
         if len(plan) == 2: unwrapped = wrapping.unwrap(speckles,(plan[0],plan[1],(N/2,M/2)))
@@ -82,12 +253,12 @@ def rot_sym(speckles,plan=None,components=None,cosines=None):
     if plan == None: unwrapped = wrapping.unwrap(speckles,(0,R/2,(N/2,M/2)))
         
     # autocorrelate the unwrapped speckle. normalize each row individually.
-    autocorrelation = crosscorr.autocorr(unwrapped,axes=(1,))
-    for row,row_data in enumerate(autocorrelation.shape[0]):
+    autocorrelation = np.fft.fftshift(crosscorr.autocorr(unwrapped,axes=(1,)))
+    for row,row_data in enumerate(autocorrelation):
         autocorrelation[row] = row_data*(1./abs(row_data).max())
     
     # generate components and cosines if necessary
-    if components == None: components = numpy.arange(2,20,2).astype('float')
+    if components == None: components = np.arange(2,20,2).astype('float')
     if cosines == None: cosines = make_cosines(components,len(autocorrelation[0]))
     
     # run cosine decomposition
--- speckle/wrapping.py
+++ speckle/wrapping.py
@@ -3,117 +3,106 @@
 Author: Daniel Parks (dparks@uoregon.edu)
 """
 
-import numpy
+import numpy as np
 
 def unwrap_plan(r,R,center,max_angle=None):
     """ Make the array which constitutes the unwrap plan. This is then passed
-    to the actual unwrap function along with the array to be unwrapped.
-    Generating a separate plan results in serious speed improvements if the
-    unwrap plan is the same.
-
+    to the actual unwrap function along with the array to be unwrapped. Generating
+    a separate plan results in serious speed improvements if the unwrap plan
+    is always the same.
+    
     Required inputs:
     r: the interior radius if the unwrapping annulus. float or int.
     R: the exterior radius of the unwrapping annulus. float or int.
     center: a 2-tuple of integers giving the center of the annulus.
-
+    
     Optional input:
     max_angle: the portion of the azimuthal coordinate to be unwrapped.
-        Default is 2pi.
+            default is 2pi.
 
     Returns:
     plan - an array object that contains the unwrapping plan.
     """
     assert isinstance(r, (float, int)), "r must be int or float"
     assert isinstance(R, (float, int)), "R must be int or float"
-    assert r >= 0, "r must be >= 0"
     assert isinstance(center, (list, tuple, set)) and len(center) == 2, "center must be 2-tuple"
     # don't bother checking if center is int, just cast it. Found out that numpy.int64 type is not considered IntType
     #center = [int(i) for i in center]
+    
+    ur,uR = min([r,R]),max([r,R])
+    assert ur > 0, "inner radius must be > 0"
 
-    if max_angle == None: max_angle = 2*numpy.pi
+    if max_angle == None: max_angle = 2*scipy.pi
     assert isinstance(max_angle, (float, int)), "max_angle must be float"
     max_angle = float(max_angle)
 
     # setup up polar arrays
-    R2,R1,cx,cy,MaxAngle = R,r,center[1],center[0],max_angle
+    ucols = int(max_angle*uR)
+    r,c = np.indices((uR-ur,ucols),float)
+    phi = np.ones_like(r)*np.arange(ucols)*max_angle/ucols
+    r += ur
     
-    cMax = int(MaxAngle*R2)
-    RTable, CTable = numpy.indices((R2-R1,cMax),float)
-    RTable += R1
-    PhiTable = numpy.ones_like(RTable)*numpy.arange(cMax)*MaxAngle/cMax
-
-    # now form the map
-    Rows,Cols = RTable.shape
-    N = Rows*Cols
-    
-    # basic trigonometry:
-    # x = x0+r*cos(Theta)
-    # y = y0+r*sin(Theta)
-    x0Table = cx+RTable*numpy.cos(PhiTable)
-    y0Table = cy+RTable*numpy.sin(PhiTable)
-    
-    # unravel to make 1-d (For speed? Why did I do this? Is this the required format?)
-    x0Table = numpy.ravel(x0Table)
-    y0Table = numpy.ravel(y0Table)
+    x = np.ravel(center[1]+r*np.cos(phi))
+    y = np.ravel(center[0]+r*np.sin(phi))
+    l = len(x)
     
     # combine into the plan array
     # Plan format:
     # [[y0,y1,y2...R],[x0,x1,x2...r]]
-    Plan = numpy.zeros((2,N+1),float)
-    Plan[0,:-1] = y0Table
-    Plan[1,:-1] = x0Table
+    plan = np.zeros((2,l),float)
+    plan[0,:-1] = x
+    plan[1,:-1] = x
+    np.mod(x0Table+N/2,N),np.mod(y0Table+N/2,N),Cols
     
     # very last entries in the plan are the radii. this means the plan contains all information for unwrapping.
-    Plan[0,-1] = R
-    Plan[1,-1] = r
+    Plan[0,-1] = uR
+    Plan[1,-1] = ur
 
     return Plan
 
 def wrap_plan(r,R,max_angle=None):
-    """ Generate a plan to rewrap an array from polar coordinates into cartesian
-    coordinates. Rewrapped array will be returned with coordinate center at
-    array center.
+    """ Generate a plan to rewrap an array from polar coordinates into cartesian coordinates.
+    Rewrapped array will be returned with coordinate center at array center.
     
     Required input:
     r: the inner raidus of the data to be wrapped.  int or float
     R: the outer radius of the data to be wrapped. int or float.
     
     Optional input:
-    max_angle: if when unwrapping you used a max_angle other than the default
-        value of 2*pi it should be resupplied here. radians.
-
+    max_angle: if when unwrapping you used a max_angle other than the default value of 2*pi it should be resupplied here. radians.
+    
     Returns:
-    plan: a ndarray plan of coordinate wrapping map. To be used in wrap().
+    plan - a ndarray plan of coordinate wrapping map.  To be used in wrap_with_plan.
     """
     assert isinstance(R, (int, float)), "R must be float or int"
     assert isinstance(r, (int, float)), "r must be float or int"
-    if max_angle == None: max_angle = 2*numpy.pi
+    if max_angle == None: max_angle = 2*scipy.pi
     assert isinstance(max_angle,float), "max_angle must be float"
     
     R1 = max([r,R])
     R2 = min([r,R])
  
     # setup the cartesian arrays
-    yArray,xArray = numpy.indices((2*R1,2*R1),float)-R1
-    RArray = numpy.sqrt(yArray**2+xArray**2)-R2
-    PhiArray = numpy.mod(numpy.angle(xArray+complex(0,1)*yArray)+2*numpy.pi,2*numpy.pi)
-    PhiArray *= 1./PhiArray.max()*int(2*numpy.pi*R-1)
+    yArray,xArray = scipy.indices((2*R1,2*R1),float)-R1
+    RArray = scipy.sqrt(yArray**2+xArray**2)-R2
+    PhiArray = scipy.mod(scipy.angle(xArray+complex(0,1)*yArray)+2*scipy.pi,2*scipy.pi)
+    PhiArray *= 1./PhiArray.max()*int(2*scipy.pi*R-1)
 
     # put them in the plan
-    Plan = numpy.zeros((2,len(yArray),len(yArray[0])),float)
+    Plan = scipy.zeros((2,len(yArray),len(yArray[0])),float)
     Plan[0] = RArray
     Plan[1] = PhiArray
     
     return Plan
 
 def unwrap(array,plan,interpolation_order=3):
-    """ Given an array and a plan, unwrap an array into polar coordinates.
+    """ Given an array and a pre-generated plan, unwrap an array into polar coordinates
     
     Required input:
     array: data to be unwrapped. must be numpy ndarray
     plan or (r, R, center): Either a description of unwrapping, obtained by
-        invoking unwrap_plan(), or the 3-tuple (r, R, center).  The latter
-        method is useful if you only need to call unwrap() once.
+        invoking unwrap_plan(), or the 3-tuple (r, R, center).  The latter method
+        is useful if you only need to call unwrap() once.
     
     Optional input:
     interpolation_order: coordinate transformations require interpolation.
@@ -123,13 +112,13 @@ def unwrap(array,plan,interpolation_order=3):
                         4 and 5 order is also possible
 
     returns:
-    unwrapped_array: the unwrapped array. The size of this is (R-r,2*pi*R)
+    unwrapped_array: the unwrapped array. The size of this is (R-r,xxx)
     """
     import scipy.ndimage
 
-    assert isinstance(array,numpy.ndarray),  "input data must be ndarray"
+    assert isinstance(array,scipy.ndarray),  "input data must be ndarray"
     assert interpolation_order in range(6), "interpolation order must be 0-5"
-    if not isinstance(plan,numpy.ndarray):
+    if not isinstance(plan,scipy.ndarray):
         assert len(plan) == 3, "unwrap plan must be a len 2 list/tuple/set or a ndarray"
         r, R, center = plan
         plan = unwrap_plan(r, R, center)
@@ -153,16 +142,13 @@ def unwrap(array,plan,interpolation_order=3):
     return unwrapped
 
 def wrap(array,plan,interpolation_order=3):
-    """ Wraps data from polar coordinates into cartesian coordinates. A plan
-    must be supplied. This is basically just a wrapper to scipy.ndimage.map_coordinates
-    with all the real work being done in generating the plan. The plan can also
-    be a 2-tuple of the inner/outer radii (r, R).
-
+    """ Wraps data from polar coordinates into cartesian coordinates. A plan must be supplied.
+    This is basically just a wrapper to scipy.ndimage.map_coordinates with all the real work
+    being done in generating the plan.
+    
     Required input:
-    array: data in polar coordinates (y axis is radial coordinate, x axis
-        is angular coordinate)
-    plan or (r, R): Either a description of unwrapping obtained by calling
-        wrap_plan(), or a 2-tuple (r, R).
+    array: data in polar coordinates (y axis is radial coordinate, x axis is angular coordinate)
+    plan or (r, R): Either a description of unwrapping obtained by calling wrap_plan(), or a 2-tuple (r, R).
 
     Optional input:
     interpolation_order: coordinate transformations require interpolation.
@@ -171,11 +157,12 @@ def wrap(array,plan,interpolation_order=3):
                         3 is cubic/bicubic (default)
                         4 and 5 order is also possible
     """
+    import scipy
     import scipy.ndimage
     
-    assert isinstance(array,numpy.ndarray),  "input data must be ndarray"
+    assert isinstance(array,scipy.ndarray),  "input data must be ndarray"
     assert interpolation_order in range(6), "interpolation order must be 0-5"
-    if isinstance(plan, numpy.ndarray):
+    if isinstance(plan, scipy.ndarray):
         pass
     else:
         assert len(plan) == 2, "plan must be len-2 tuple/set/list or ndarray"
