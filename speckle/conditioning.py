516df4a21116a613907a49f6d1dc6c3a6cc72cc7
Keoki Seu <kaseu@lbl.gov>
2012-07-05 15:33:49 -0700

    implement match_counts() to match the number of counts between two arrays with a mask.
    other changes:
    - DFT is used once, so remove the bindings and call fft2 directly.
    - add LOTS of documentation
    - make lots of the code and logic simplier, ie use average instead of sum()/square.
    - rename variables so that they're much more readable

--- speckle/conditioning.py
+++ speckle/conditioning.py
@@ -2,26 +2,29 @@
 for proper analysis. The goal is to be able to transparently handle experimental
 and simulated data through the same analytical functions.
 
-Author: Daniel Parks (dhparks@lbl.gov)"""
+Author: Daniel Parks (dhparks@lbl.gov)
+Author: Keoki Seu (kaseu@lbl.gov)
+"""
 import numpy
-DFT = numpy.fft.fft2
-IDFT = numpy.fft.ifft2
 
 def remove_dust(data,dust_mask,dust_plan=None):
-    """ Attempts to remove dust and burn marks from CCD images by interpolating in regions marked as
-    defective in a plan file.
+    """ Attempts to remove dust and burn marks from CCD images by interpolating
+    in regions marked as defective in a plan file.
     
     Requires:
-        data - the data from which dust will be removed. ndarray or path to some sort of data
-        dust_mask - a binary array describing from which pixels dust must be removed
-        
+        data - the data from which dust will be removed. 2d or 3d ndarray.
+        dust_mask - a binary array describing from which pixels dust must be
+            removed.
+
     Optional input:
-        plan - generated from dust_mask; this can only be supplied as an argument if remove_dust has
-        been previously run and plan returned then as output
-        
+        plan - generated from dust_mask; this can only be supplied as an
+            argument if remove_dust has been previously run and plan returned
+            then as output.
+
     Returns:
-        0 - fixed data
-        1 - plan unique to dust_mask which can be passed again for re-use."""
+        data - the data array with the dust spots removed.
+        plan - plan unique to dust_mask which can be passed again for re-use.
+    """
 
     from scipy.signal import cspline1d, cspline1d_eval
 
@@ -133,8 +136,26 @@ def plan_remove_dust(Mask):
     # elements of a list without order preservation which doesn't matter for this purpose
     return tuple(set(PixelIDStrings))
 
-def subtract_background(data,dark=None,x=20,scale=1):
-    """Subtract a background file. The DC component of both files is subtracted first."""
+def subtract_background(data, dark=None, x=20, scale=1, abs_val=True):
+    """Subtract a background file. The DC component of both files is subtracted
+    first. the DC component is calculated by averaging pixels 0:x in both
+    directions.
+
+    arguments:
+        data - data to subtract.
+        dark - dark file. Defaults to None.  In this case only the DC component
+            is subtracted.
+        x - amount from the data edge that should be used for DC component
+            averaging. The dc component is average(data[0:x, 0:x]). Defaults to
+            20 pixels.
+        scale - amount to scale up the dark image before subtraction. Defaults
+            to 1 (no scaling).
+        abs_val - Weather the absolute value of the data should be returned.
+            Defaults to True.
+
+    returns:
+        data - the background-subtracted data.
+    """
 
     # check types
     assert isinstance(data,numpy.ndarray), "data must be ndarray"
@@ -143,49 +164,52 @@ def subtract_background(data,dark=None,x=20,scale=1):
     if isinstance(dark,numpy.ndarray):
         assert dark.ndim == 2, "dark must be 2d"
         assert data.shape[-2:] == dark.shape, "data and dark must be same shape"
+    assert abs_val in (bool, int), "abs_val must be boolean-evaluable"
     
     # subtract DC component from data
     if data.ndim == 2:
-        dc = float(numpy.sum(data[0:x,0:x])/x**2)
+        dc = numpy.average(data[0:x,0:x])
         data = abs(data-dc)
     if data.ndim == 3:
         for n in range(data.shape[0]):
-            dc = float(numpy.sum(data[n,0:x,0:x])/x**2)
+            dc = numpy.average(data[n,0:x,0:x])
             data[n] = abs(data[n]-dc)
         
     # dark subtraction can be broadcast to all frames of data so no need to check ndim
-    if dark == None:
-        pass
-    else:
-        bgdc = float(numpy.sum(dark[0:x,0:x])/x**2)
-        dark = abs(dark-bgdc)
-        data = abs(data-dark*scale)
-        
-    return data
+    if dark is not None:
+        dark = dark-numpy.average(dark[0:x,0:x])
+        data = data-dark*scale
 
-def hot_pixels(data,i=1,t=2):
-    """A basic hot pixel remover which uses numpy.medfilt to define hot pixels as those
-    which exceed a certain multiple of the local median. Slow for large arrays; this operation
-    would probably benefit a great deal from GPU acceleration.
+    if abs_val:
+        return abs(data)
+    else:
+        return data
+
+def hot_pixels(data, iterations=1, threshold=2):
+    """A basic hot pixel remover which uses numpy.medfilt to define hot pixels
+    as those which exceed a certain multiple of the local median.
     
     Required:
-        data -- 2d or 3d array from which hot pixels will be removed
+        data - 2d or 3d array from which hot pixels will be removed
         
     Optional:
-        i -- number of iterations to run the smoother. Default is 1.
-        t -- threshold to specify when pixels are hot. When a pixel has
-            value greater than t*median, it is replaced by the median.
+        iterations - number of iterations to run the smoother. Default is 1.
+        threshold - threshold to specify when pixels are hot. When a pixel has
+            value greater than threshold*median, it is replaced by the median.
             Default is 2.
-    
+
+    Returns:
+        data - The data wih the hot pixels that meet the threshold removed.
     """
-    
+    #  This is slow for large arrays; this operation would probably benefit a great deal from GPU acceleration.
+
     from scipy.signal import medfilt
     
     # check types
     assert isinstance(data,numpy.ndarray), "data must be ndarray"
     assert data.ndim in (2,3), "data must be 2d or 3d"
-    assert isinstance(i,int), "number of iterations must be integer"
-    assert isinstance(t, (int,float)), "threshold must be float or int"
+    assert isinstance(iterations,int), "number of iterations must be integer"
+    assert isinstance(threshold, (int,float)), "threshold must be float or int"
     
     was_2d = False
     if data.ndim == 2:
@@ -193,38 +217,44 @@ def hot_pixels(data,i=1,t=2):
         data.shape = (1,data.shape[0],data.shape[1])
     for z,frame in enumerate(data):
 
-        for m in range(i):
+        for m in range(iterations):
             median = medfilt(frame)+.1
-            Q = numpy.where(frame/median > t,1,0)
+            Q = numpy.where(frame/median > threshold,1,0)
             data[z] = frame*(1-Q)+median*Q
 
     if was_2d: data = data[0]
     return data
     
 def align_frames(data,align_to=None,region=None,use_mag_only=False,return_type='data'):
-    
     """ Align a set of data frames by FFT/cross-correlation method.
-    
-    Required input:
-    data -- A 2d or 3d ndarray. Probably this needs to be real-valued, not complex.
-    
+
+    Inputs:
+        data - A 2d or 3d ndarray.
+
     Optional arguments:
-        align_to -- A 2d array used as the alignment reference. If None, data must
-                    be 3d and the first frame of data will be the reference. Default is None
-                
-        region -- A 2d mask which specifies which data to use in the cross correlation. If None,
-                  all pixels will contribute equally to the alignment. Default is None.
-        
-        use_mag_only -- Do alignment using only the magnitude component of data. Default is False.
-                  
-        return_type -- align_frames is called from multiple places, and expectations of what is
-                       returned vary. Returned can be aligned data, aligned and summed data, or
-                       just the alignment coordinates; keywords for these are 'data', 'sum', and
-                       'coordinates', respectively. Default is 'data'.
-              
-    Returns: an array of shape and dtype identical to data, data summed along the 0 axis, or an
-             array of alignment coordinates depending on return_type argument."""
-             
+        align_to - A 2d array used as the alignment reference. If None, data
+            must be 3d and the first frame of data will be the reference.
+            Default is None
+        region - A 2d mask which specifies which data to use in the cross
+            correlation. If None, all pixels will contribute equally to the
+            alignment. Default is None.
+        use_mag_only - Align using only the magnitude component of data. Default
+            is False.
+        return_type - align_frames is called from multiple places, and
+            expectations of what is returned vary. Returned can be aligned data,
+            aligned and summed data, or just the alignment coordinates; keywords
+            for these are 'data', 'sum', and 'coordinates', respectively.
+            Default is 'data'.
+
+    Returns:
+        result - The result depends on the input return_type. If return_type is
+        set to:
+            'data' - An aligned array of shape and dtype identical to data, or
+            'sum' - the the summed array, or
+            'coordinates' - the coordinates that the arrays need to be rolled
+                in order to align them.
+    """
+
     from . import crosscorr
     
     # check types
@@ -234,7 +264,7 @@ def align_frames(data,align_to=None,region=None,use_mag_only=False,return_type='
     assert use_mag_only in (0,1,True,False),                      "use_mag_only must be boolean-evaluable"
     assert return_type in ('data','sum','coordinates'),           "return_type must be 'data', 'sum', or 'coordinates'; 'data' is default"
     if data.ndim == 2: assert isinstance(align_to,numpy.ndarray), "data is 2d; need an explicit alignment reference"
-    if data.ndim == 2 and return_type == 'sum': print             "summing 2d data is non-sensical" # not an assert!
+#    if data.ndim == 2 and return_type == 'sum': print             "summing 2d data is non-sensical" # not an assert!
     
     # define some simple helper functions to improve readability
     rolls = lambda d, r0, r1: numpy.roll(numpy.roll(d,r0,axis=0),r1,axis=1)
@@ -254,24 +284,24 @@ def align_frames(data,align_to=None,region=None,use_mag_only=False,return_type='
     
     # check some more assumptions
     if region != None:   assert region.shape == (rows,cols),    "region and data frames must be same shape"
-    if align_to != None: assert align_to.shape == (rows,cols),  "align_to and data frames must be same shape"
 
     # set up explicit align_to in case of None
-    if align_to == None: align_to = data[0]
+    if align_to == None:
+        align_to = data[0]
+    else:
+        assert align_to.shape == (rows,cols),  "align_to and data frames must be same shape"
     
     # for speed, precompute the reference dft
-    ref = DFT(prep(align_to))
+    ref = numpy.fft.fft2(prep(align_to))
     
     # get the alignment coordinates for each frame in data by the argmax of the cross
     # correlation with the reference
     coordinates = numpy.zeros((frames,2),int)
-    irows,icols = numpy.indices((rows,cols),float)
     for n,frame in enumerate(data):
         coordinates[n] = crosscorr.alignment_coordinates(prep(frame),ref,already_fft=(1,))
         
     # now return the data according to return_type
     if return_type == 'coordinates': return coordinates
-    
     # align data frames by rolling
     for n in range(frames):
         rr, rc = coordinates[n]
@@ -282,3 +312,106 @@ def align_frames(data,align_to=None,region=None,use_mag_only=False,return_type='
         return data
     
     if return_type == 'sum': return numpy.sum(data,axis=0) 
+
+def match_counts(img1, img2, region=None, nparam=3):
+    """ Match the counts between two images. There are options to match in a
+        region of interest and the number of fitting parameters to be used.
+
+    arguments:
+        img1 - first image to match. Must be 2d and the same size as img2.
+        img2 - second image to match. Must be 2d and the same size as img1.
+        region - mask image that is >=1 in the pixels/areas that are to be used
+            for matching.  Defaults to None, which uses the entire image.
+        nparam - Number of fitting parameters used to match counts. The fit
+            function used depends on nparam:
+                1 - min(img1 - s*img2) with one parameter, s
+                2 - min(img1 - s*(img2 - d2)), with two parameters, (s, d2)
+                3 - min(img1 - s*img2 + (s*d2 - d1)) with (s, d1, d2)
+            The default is 3 parameters, which accounts for different
+            backgrounds of the two images (d1, d2) and scaling s.
+
+    returns:
+        img2 - a scaled img2 such that the counts in region match.
+    """
+    import scipy.optimize
+    def diff3(c, img1, img2, region):
+        """ minimize (I1 - d1) - s(I2-d2)
+            = I1 - s*I2 + (s*d2 - d1)
+        """
+        (s, d1, d2) = c
+        if d1 < 0 or d2 < 0:
+            return 1e30
+        dif = region*(img1 - d1 - s*(img2 - d2))**2
+        return dif.sum()
+
+    def diff2(c, img1, img2, region):
+        """ minimize I1 - s(I2-d2)
+            = I1 - s*(I2 - d2)
+        """
+        (s, d2) = c
+        if d2 < 0:
+            return 1e30
+        dif = region*(img1 - s*(img2 - d2))**2
+        return dif.sum()
+
+    def diff1(c, img1, img2, region):
+        """ minimize I1 - s*I2
+            = I1 - s*I2
+        """
+        (s) = c
+        dif = region*(img1 - s*img2)**2
+        return dif.sum()
+
+    assert isinstance(img1, numpy.ndarray) and img1.ndim == 2, "img1 must be a 2d ndarray."
+    assert isinstance(img2, numpy.ndarray) and img2.ndim == 2, "img2 must be a 2d ndarray."
+    assert img1.shape == img2.shape, "(img1, img2) must be the same shape"
+
+    if not (type(nparam) == int and nparam in (1,2,3)):
+        nparam = 3 
+
+    if type(region) != numpy.ndarray and region == False:
+        region = numpy.ones_like(img1)
+    else:
+        # convert region to 1/0 just to be sure
+        region = numpy.where(region >= 1, 1, 0) 
+
+    if region.sum() == 0:
+        print("***** match_counts: Region of interest is empty! *****")
+        return img2
+
+    d1 = img1[0,0]
+    d2 = img2[0,0]
+    c0guess = (img1-d1)/(img2-d2)
+    # remove the infinities and nans.  we get these from img2-d2 =0
+    c0guess = numpy.where(numpy.isfinite(c0guess), c0guess, 0)
+
+    s = (c0guess*region).sum()/region.sum()
+
+    if nparam == 1:
+        c = numpy.array([s])
+        diff = diff1
+        paramstr = "s=%1.2f"
+        funcstr = "img1-s*img2 (%d parameter)" % nparam
+    elif nparam == 2:
+        c = numpy.array([s, d2])
+        diff = diff2
+        paramstr = "s=%1.2f, d2=%1.2f"
+        funcstr = "img1-s*(img2-d2) (%d parameters)" % nparam
+    else: # assume 3 parameter fit
+        c = numpy.array([s, d1, d2])
+        diff = diff3
+        paramstr = "s=%1.2f, d1=%1.2f, d2=%1.2f"
+        funcstr = "(img1-d1)-s*(img2-d2) (%d parameters)" % nparam
+
+    print("minimizing %s.\nInitial guess: %s." % (funcstr, paramstr % tuple(c)))
+
+    x = scipy.optimize.fmin(diff, c, args=(img1, img2, region), disp=False)
+
+    print("Final result: %s." % (paramstr % tuple(x)))
+
+    if nparam == 1:
+        return x[0]*img2
+    elif nparam == 2:
+        return x[0]*(img2 - x[1])
+    else:
+        return x[0]*(img2 - x[2]) + x[1]
