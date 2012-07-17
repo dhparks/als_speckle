7f11905d346b987da000ee787daaa42971f6f23a
Daniel Parks <dparks@uoregon.edu>
2012-07-17 16:28:04 -0700

    rewrite part of merge to avoid in-place change

--- speckle/conditioning.py
+++ speckle/conditioning.py
@@ -587,15 +587,17 @@ def merge(data_to, data_from, fill_region, fit_region=None, width=10):
         fill_region = io.open(fill_region)
     if isinstance(fit_region, str):
         fit_region = io.open(fit_region)
-    
+        
+    # make the blender
     assert fill_region.shape == data_to.shape, "fill_region and data must be same shape"
+    blender1 = make_blender(fill_region,width)
+    
+    # scale the data to reconcile acquisition times etc
     if fit_region != None:
         assert fit_region.shape == data_to.shape, "fit_region and data must be same shape"
-        # scale the data to reconcile acquisition times etc
-        data_from = match_counts(data_to, data_from, region=fit_region)
-
-    # make the blender
-    blender1 = make_blender(fill_region,width)
+        scaled = match_counts(data_to, data_from, region=fit_region)
+    if fit_region == None:
+        scaled = numpy.copy(data_from)
 
     # return the merged data
-    return data_to*blender1 + data_from*(1-blender1) 
+    return data_to*blender1 + scaled*(1-blender1) 
