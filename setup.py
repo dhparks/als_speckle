c2ec727ec04f2f8f2bec87e3ce933f5821d48901
Keoki Seu <kaseu@lbl.gov>
2012-07-26 15:33:41 -0700

    Don't bother globbing the .cl files.  we don't need to. Instead use package_data

--- setup.py
+++ setup.py
@@ -2,20 +2,13 @@ try:
     from setuptools.core import setup
 except ImportError:
     from distutils.core import setup
-    
-dependencies = ['scipy', 'numpy']
-
-# run glob on the gpu/kernels directory to get a list
-# of all the .cl files to copy
-import glob
-kernel_files = glob.glob('./speckle/gpu/kernels/*.cl')
 
 setup(
     name='speckle',
     version='0.3',
-    packages=['speckle','speckle.gpu','speckle.simulation'],
-    requires = dependencies,
-    data_files = ['speckle/gpu/kernels', kernel_files, 'test', 'examples'],
+    packages=['speckle'],
+    requires = ['scipy', 'numpy'],
+    package_data = { 'speckle': ['gpu/kernels/*.cl']},
     author = "Daniel Parks, Keoki Seu",
     author_email = "dhparks@lbl.gov, kaseu@lbl.gov",
     description = "Library for speckle analysis at Beamline 12.0.2 at the Advanced Light Source",
