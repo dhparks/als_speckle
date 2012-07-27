a2702dd44ae2b813ceda0e0a5daa79a75d6179b0
Keoki Seu <kaseu@lbl.gov>
2012-07-26 17:52:01 -0700

    correctly use setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-try:
-    from setuptools.core import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
 setup(
     name='speckle',
     version='0.3',
-    packages=['speckle'],
+    packages=[ 'speckle', 'speckle.gpu', 'speckle.simulation' ],
     requires = ['scipy', 'numpy'],
-    package_data = { 'speckle': ['gpu/kernels/*.cl']},
+    package_data = { 'speckle.gpu': ['kernels/*.cl']},
     author = "Daniel Parks, Keoki Seu",
     author_email = "dhparks@lbl.gov, kaseu@lbl.gov",
     description = "Library for speckle analysis at Beamline 12.0.2 at the Advanced Light Source",
