c95127badcc34b16c9f23063eec035c44adb5be9
Keoki Seu <kaseu@lbl.gov>
2012-07-28 16:43:33 -0700

    Use system curl instead of wget to grab data.  not everyone has wget.

--- examples/get-data.sh
+++ examples/get-data.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-wget "http://dl.dropbox.com/u/27355570/data.zip"
+# Enthought distributes a curl in /Libraray/Frameworks/EPD, but it does not
+# appear to work.  Default to using system cURL.
+/usr/bin/curl -O http://dl.dropbox.com/u/27355570/data.zip
 unzip data.zip -d data
