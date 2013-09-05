In io, include an option for save to not append the component name to the end of a file. In phasing, fix a bad bug for the calculation of the Fourier divisor in the cpu code-path. In wrapping, add support for unwrapping of 3d arrays (apply same transformation to each frame) and add support for a number of unwrapped columns < 2*pi*R within the unwrapping plan itself; update wrapping_example to match the new capabilities.

speckle/phasing.py
@@ -791,7 +791,7 @@ class phasing(common):
             if use_gpu: self._cl_abs(psi,div)
             else:
                 if have_numexpr: div = numexpr.evaluate("abs(psi)")
-                else:            div = abs(psi)
+                else:            div = np.abs(psi)
             
             # step two: if a transverse coherence estimate has been supplied
             # through the ipsf, use it to blur the modulus.
@@ -804,7 +804,7 @@ class phasing(common):
                     # square
                     self._cl_mult(div,div,div)
                 
-                    # convolve
+                    # convolve. abs fixes leakage into imag component
                     self._convolvef(div,psf,div)
                     self._cl_abs(div,div)
 
@@ -814,15 +814,15 @@ class phasing(common):
                 else:
                     
                     if have_numexpr:
-                        div  = numexpr.evaluate("psi**2")
+                        div  = numexpr.evaluate("div**2")
                         div2 = np.fft.fft2(div)
                         div  = numexpr.evaluate("div2*psf")
                         div  = np.fft.ifft2(div)
                         div  = numexpr.evaluate("sqrt(abs(div))")
                     else:
-                        div = psi**2
+                        div = div**2
                         div = np.fft.ifft2(np.fft.fft2(div)*psf)
-                        div = np.sqrt(abs(div))
+                        div = np.sqrt(np.abs(div))
                     
             # step three: if a longitudinal coherence estimate has been supplied
             # through the spectrum, use it to blur the modulus.

speckle/wrapping.py
@@ -5,7 +5,7 @@ Author: Daniel Parks (dparks@uoregon.edu)
 
 import numpy as np
 
-def unwrap_plan(r, R, center, modulo=None, target='cpu'):
+def unwrap_plan(r, R, center, columns=None, modulo=None, target='cpu'):
     """ Make the array which constitutes the unwrap plan. This is then passed
     to the actual unwrap function along with the array to be unwrapped.
     Generating a separate plan results in serious speed improvements if the
@@ -15,6 +15,10 @@ def unwrap_plan(r, R, center, modulo=None, target='cpu'):
         r - the interior radius if the unwrapping annulus. float or int.
         R - the exterior radius of the unwrapping annulus. float or int.
         center - a 2-tuple of integers giving the center of the annulus.
+        columns - number of columns in the unwrapped array. By default, the data
+            is not resampled after unwrapping and the number of columns is
+            2*pi*R. For large images or 3d arrays, this number of columns might
+            be too large.
         modulo - if put the coordinates through a modulo operation of this
             value. This is intended for use in unwrapping machine-centered
             speckle without requiring a fftshift. Generally, this should be the
@@ -37,7 +41,8 @@ def unwrap_plan(r, R, center, modulo=None, target='cpu'):
     assert ur >= 0, "inner radius must be >= 0"
 
     # setup up polar arrays
-    ucols = int(2*np.pi*uR)
+    if columns == None: ucols = int(2*np.pi*uR)
+    else: ucols = columns
     r,c = np.indices((uR-ur,ucols),float)
     phi = np.ones_like(r)*np.arange(ucols)*2*np.pi/ucols
     r += ur
@@ -116,6 +121,7 @@ def unwrap(array,plan,interpolation_order=3,modulo=None):
     import scipy.ndimage
 
     assert isinstance(array,np.ndarray),  "input data must be ndarray"
+    assert array.ndim in (2,3), "input data must be 2d or 3d"
     assert interpolation_order in range(6), "interpolation order must be 0-5"
     if not isinstance(plan,np.ndarray):
         assert len(plan) == 3, "unwrap plan must be a len 2 list/tuple/set or a ndarray"
@@ -125,19 +131,30 @@ def unwrap(array,plan,interpolation_order=3,modulo=None):
     # separate plan into coordinate map and (r,R)
     R,r = plan[:,-1]
     plan = plan[:,:-1]
+    rows, cols = R-r, len(plan[0])/(R-r)
     
-    l1,l2 = array.shape
+    # cast dimensions
+    was_2d = True
+    if array.ndim == 3: was_2d = False
+    if array.ndim == 2: array.shape = (1,)+array.shape
+    
+    l0,l1,l2 = array.shape
     ymax = plan[0].max()
     xmax = plan[1].max()
     
     assert ymax <= l1 and xmax <= l2, "max unwrapped coordinates fall outside array.\ndid you use the correct plan for this array size?"
-    
-    # unwrap
-    unwrapped = scipy.ndimage.map_coordinates(array,plan,order=interpolation_order)
-    
-    # the unwrapped version is 1d. reshape into the correct 2d arrangement (this is why the plan needs r and R)
-    unwrapped.shape = (R-r,len(plan[0])/(R-r))
-    plan.shape = (2,R-r,len(plan[0])/(R-r))
+
+    # unwrap each frame. reshape, then store in unwrapped
+    unwrapped = np.zeros((array.shape[0],int(rows),int(cols)),np.float32)
+    for nf in range(array.shape[0]):
+        uw = scipy.ndimage.map_coordinates(array[nf],plan,order=interpolation_order)
+        uw.shape = (R-r,len(plan[0])/(R-r))
+        unwrapped[nf] = uw
+
+    # restore the shape of the array if it was 2d
+    if was_2d:
+        array.shape = array.shape[1:]
+        unwrapped   = unwrapped[0]
 
     return unwrapped
 